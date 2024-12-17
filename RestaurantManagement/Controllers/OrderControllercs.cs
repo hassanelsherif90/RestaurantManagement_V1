e8bf6546ba67261ac41058c58683719c05434bd2
hassan elsherif <hassanelsherif90@gmail.com>
2024-12-17 16:40:44 +0300

    Create order

---
+++ RestaurantManagement/Controllers/OrderControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantManagement.Controllers;
+
+public class OrderControllercs : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
